df49008d59aa4ac3a9580d06572eb0e11a4e6d15
Sharon-Mosher <64615121+Sharon-Mosher@users.noreply.github.com>
2020-07-20 19:21:41 -0400

    fixed bug

--- components/Tabs.js
+++ components/Tabs.js
@@ -10,25 +10,26 @@
 //
 // NOTE: you do _not_ need to install axios as it's included in the HTML via script element
 
-axios.get('https://lambda-times-backend.herokuapp.com/topics').then(response => {
-    response.data.topics.forEach(item => {
-        
-        tabHolder.appendChild(tabMaker(item));
-    })
-    
-    console.log(response.data.topics); 
+axios
+  .get("https://lambda-times-backend.herokuapp.com/topics")
+  .then((response) => {
+    response.data.topics.forEach((item) => {
+      tabHolder.appendChild(tabMaker(item));
+    });
 
-}).catch(error => {
-    console.log(error); 
-}); 
+    console.log(response.data.topics);
+  })
+  .catch((error) => {
+    console.log(error);
+  });
 
 function tabMaker(topic) {
-    const tab = document.createElement('div'); 
-    tab.classList.add('tab'); 
-    tab.textContent = topic; 
-    return tab;
-} 
+  const tab = document.createElement("div");
+  tab.classList.add("tab");
+  tab.textContent = topic;
+  return tab;
+}
 
-const tabHolder = document.querySelector('div.topics'); 
+const tabHolder = document.querySelector("div.topics");
 
- console.log(tabHolder);  
+console.log(tabHolder);
